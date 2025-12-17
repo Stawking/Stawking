262cd93e3a20f982811a5a797d1298266720484f
Angel Cabrera <109184891+Stawking@users.noreply.github.com>
2025-12-17 13:23:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import linecache
 
 class WordList:
     FILE_PATH = 'words.txt'
-    LINE_COUNT = 5
-    @staticmethod
-    def getword() -> str: 
-        word = linecache.getline(WordList.FILE_PATH, random.randrange(1,WordList.LINE_COUNT))
+    LINE_COUNT = 854
+    
+    @classmethod
+    def getword(cls) -> str: 
+        word = linecache.getline(cls.FILE_PATH, random.randrange(1,cls.LINE_COUNT))
         linecache.clearcache()
-        if word:
-            return word.strip()
-        else:
-            print("File/Line does not exist!")
-            return 
+        return word.strip()
         
 print(WordList.getword())
         
